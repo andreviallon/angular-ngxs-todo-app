8b33671e0e8b8db1116221d4f7e67d9c9f09fc80
Andre Viallon <viallon.andre@gmail.com>
2019-11-07 16:02:48 +0100

    Generate add todo and todos list compoent

---
+++ src/app/add-todo/add-todo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.scss']
+})
+export class AddTodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodosListComponent } from './todos-list/todos-list.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosListComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule
