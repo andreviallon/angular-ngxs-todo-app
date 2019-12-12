1ed7dea6f2e8860f2f2317d6d1453d1e7e0e9777
Andre Viallon <viallon.andre@gmail.com>
2019-12-12 15:00:04 +0100

    Update styling on add todo component

--- src/stories/todos-list.stories.ts
+++ src/stories/todos-list.stories.ts
@@ -1,37 +1,42 @@
+import { Observable } from 'rxjs';
 import { TodosListComponent } from '../app/todos-list/todos-list.component';
 import { storiesOf, moduleMetadata } from '@storybook/angular';
 import { action } from '@storybook/addon-actions';
 
-function createComponent(props) {
-  return () => ({
-    component: TodosListComponent,
-    props
-  });
-}
-
 export const defaultTodos = [
-  'Todo 1',
-  'Todo 2',
-  'Todo 3'
+  { name: 'Todo 1' },
+  { name: 'Todo 2' },
+  { name: 'Todo 3' }
 ];
 
 export const props = {
-  todos$: defaultTodos,
+  todos$: new Observable(observer => { observer.next(defaultTodos) }),
   onRemoveTodo: action('Remove Todo')
+}
+
+export const withoutTodos = {
+  todos$: new Observable(observer => { observer.next([]) })
 }
 
 storiesOf('TodosList', module)
   .addDecorator(
     moduleMetadata({
       declarations: [TodosListComponent]
-    })
+    }),
   )
-  // .add('with Todos', createComponent({
-  //   todos$: defaultTodos
-  // }))
   .add('with Todos', () => {
     return {
-      template: `<todos-list [todos$]="todos" (onRemoveTodo)="onRemoveTodo($event)"></todos-list>`,
+      template: `
+      <div style="padding-top: 2rem">
+        <todos-list [todos$]="todos$" (onRemoveTodo)="onRemoveTodo($event)"></todos-list>
+      </div>
+      `,
       props
     };
+  })
+  .add('without Todos', () => {
+    return {
+      template: `<todos-list [todos$]="todos$" (onRemoveTodo)="onRemoveTodo($event)"></todos-list>`,
+      withoutTodos
+    }
   });
