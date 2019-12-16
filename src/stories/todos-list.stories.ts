8774637b1d8223324e55052c50ccaf3a1fee93b5
Andre Viallon <viallon.andre@gmail.com>
2019-12-16 20:45:45 +0100

    Update storybook check todo

--- src/stories/todos-list.stories.ts
+++ src/stories/todos-list.stories.ts
@@ -4,13 +4,14 @@ import { storiesOf, moduleMetadata } from '@storybook/angular';
 import { action } from '@storybook/addon-actions';
 
 export const defaultTodos = [
-  { name: 'Todo 1' },
-  { name: 'Todo 2' },
-  { name: 'Todo 3' }
+  { name: 'Todo 1', checked: false },
+  { name: 'Todo 2', checked: false },
+  { name: 'Todo 3', checked: true }
 ];
 
 export const props = {
   todos$: new Observable(observer => { observer.next(defaultTodos) }),
+  onCheckTodo: action('Check Todo'),
   onRemoveTodo: action('Remove Todo')
 }
 
@@ -28,7 +29,7 @@ storiesOf('TodosList', module)
     return {
       template: `
       <div style="padding-top: 2rem">
-        <todos-list [todos$]="todos$" (onRemoveTodo)="onRemoveTodo($event)"></todos-list>
+        <todos-list [todos$]="todos$" (onCheckTodo)="onCheckTodo($event)" (onRemoveTodo)="onRemoveTodo($event)"></todos-list>
       </div>
       `,
       props
